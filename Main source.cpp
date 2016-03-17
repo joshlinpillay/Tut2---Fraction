8191062a2b57ab62a81abf8f506dc848d6e98cd1
joshlinpillay <joshlinpillay12@gmail.com>
2016-03-17 18:15:39 +0200

    Final

---
+++ Main source.cpp
@@ -0,0 +1,30 @@
+// Tut2 - Fraction
+
+include<iostream>
+#include"fraction.h"
+
+using namespace std;
+
+int main()
+{
+
+	fraction sub(3, 9);
+	fraction mult(4, 13);
+	fraction div(1, 12);
+	fraction ad(5, 20);
+
+	sub = sub.subtract(ad);
+	mult = mult.Multiply(ad);
+	div = div.Divide(ad);
+	mult = mult.Add(ad);
+
+	ad.print();
+	sub.print();
+	mult.print();
+	div.print();
+
+
+	system("PAUSE");
+
+	return 0;
+}
